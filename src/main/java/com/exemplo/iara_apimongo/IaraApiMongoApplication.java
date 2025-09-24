865aa35d0b7fb95cf158565425f50fa62d252b8d
Lara Dielly <laradgtech75@gmail.com>
2025-09-24 05:35:22 -0300

    Feat: Apply MVC architecture with packet separation

---
+++ src/main/java/com/exemplo/iara_apimongo/IaraApiMongoApplication.java
@@ -0,0 +1,13 @@
+package com.exemplo.iara_apimongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IaraApiMongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IaraApiMongoApplication.class, args);
+    }
+
+}
