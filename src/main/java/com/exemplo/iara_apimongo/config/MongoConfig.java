b2195edc3e2dc4cb75d9508df3810b1b90b46b04
Lara Dielly <laradgtech75@gmail.com>
2025-10-20 10:59:58 -0300

    fix: corrigir conexão com MongoDB Atlas usando DB_URI

--- src/main/java/com/exemplo/iara_apimongo/config/MongoConfig.java
+++ src/main/java/com/exemplo/iara_apimongo/config/MongoConfig.java
@@ -10,27 +10,11 @@ public class MongoConfig {
 
     @Bean
     public MongoClient mongoClient() {
-        String host = EnvConfig.get("DB_HOST");
-        String port = EnvConfig.get("DB_PORT");
-        String dbName = EnvConfig.get("DB_NAME");
-        String user = EnvConfig.get("DB_USER");
-        String pass = EnvConfig.get("DB_PASSWORD");
-
-        String connectionString;
-
-        if (user != null && !user.isEmpty() && pass != null && !pass.isEmpty()) {
-            connectionString = String.format(
-                    "mongodb://%s:%s@%s:%s/%s",
-                    user, pass, host, port, dbName
-            );
-        } else {
-            connectionString = String.format(
-                    "mongodb://%s:%s/%s",
-                    host, port, dbName
-            );
+        String uri = EnvConfig.get("MONGO_URI");
+        if (uri == null || uri.isEmpty()) {
+            throw new IllegalStateException("Variável de ambiente DB_URI não encontrada!");
         }
-
-        return MongoClients.create(connectionString);
+        return MongoClients.create(uri);
     }
-}
 
+}
