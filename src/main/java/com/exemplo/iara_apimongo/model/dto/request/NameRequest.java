8a6748b95b0c315dfbb83e46c01f032805f7c763
melpiress <melissa.foneca@germinare.org.br>
2025-11-01 10:32:38 -0300

    feat: add endpoint to get shift by name

---
+++ src/main/java/com/exemplo/iara_apimongo/model/dto/request/NameRequest.java
@@ -0,0 +1,8 @@
+package com.exemplo.iara_apimongo.model.dto.request;
+
+import lombok.Getter;
+
+@Getter
+public class NameRequest {
+    private String name;
+}
--- src/main/java/com/exemplo/iara_apimongo/repository/ShiftRepository.java
+++ src/main/java/com/exemplo/iara_apimongo/repository/ShiftRepository.java
@@ -4,6 +4,9 @@ import com.exemplo.iara_apimongo.model.database.Shift;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ShiftRepository extends MongoRepository<Shift, String> {
+    Optional<Shift> findByNameContainsIgnoreCase(String name);
 }
